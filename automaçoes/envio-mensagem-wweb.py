97bf956e3aa4568956cf63148a92722ad219c13e
AndreSobralDEV <159955496+AndreSobralDEV@users.noreply.github.com>
2024-02-24 12:19:59 -0300

    Update envio-mensagem-wweb.py

--- automaçoes/envio-mensagem-wweb.py
+++ automaçoes/envio-mensagem-wweb.py
@@ -10,15 +10,19 @@ driver.get("https://web.whatsapp.com/")
 time.sleep(60)  # Espera 10 segundos para você escanear o código QR
 
 # Encontra o campo de pesquisa e insere o nome do contato/grupo
-search_box = driver.find_element_by_xpath('//input[@class="_3FRCZ copyable-text selectable-text" and @data-tab="3"')
-search_box.send_keys("Lucas Irmão" + Keys.ENTER)
+contato = "Lucas Irmão"
+campo_pesquisa = driver.find_element_by_xpath('//div[@class="_3FRCZ copyable-text selectable-text"][@contenteditable="true"][@data-tab="3"]')
+campo_pesquisa.send_keys(contato)
+campo_pesquisa.send_keys(Keys.ENTER)
 
 # Espera um segundo para a conversa ser carregada
-time.sleep(1)
+time.sleep(5)
 
 # Encontra o campo de mensagem e envia a mensagem desejada
-message_box = driver.find_element_by_xpath('//div[@class="_2_1wd copyable-text selectable-text"][@contenteditable="true"][@data-tab="1"]')
-message_box.send_keys("Teste Python" + Keys.ENTER)
+mensagem = "Teste Python"
+campo_mensagem = driver.find_element_by_xpath('//div[@class="_3FRCZ copyable-text selectable-text"][@contenteditable="true"][@data-tab="6"]')
+campo_mensagem.send_keys(mensagem)
+campo_mensagem.send_keys(Keys.ENTER)
 
 # Fecha o navegador após o envio da mensagem
 driver.quit()
