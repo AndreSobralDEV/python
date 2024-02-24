8115d4e3304c14deb9d61144e6849b04cc9a8b19
AndreSobralDEV <159955496+AndreSobralDEV@users.noreply.github.com>
2024-02-24 10:53:57 -0300

    teste01

---
+++ automaçoes/envio-mensagem-wweb.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+# Inicializa o driver do Chrome (certifique-se de ter o ChromeDriver instalado)
+driver = webdriver.Chrome()
+
+# Abre o WhatsApp Web
+driver.get("https://web.whatsapp.com/")
+time.sleep(10)  # Espera 10 segundos para você escanear o código QR
+
+# Encontra o campo de pesquisa e insere o nome do contato/grupo
+search_box = driver.find_element_by_xpath('//div[@class="_2_1wd copyable-text selectable-text"][@contenteditable="true"][@data-tab="3"]')
+search_box.send_keys("Lucas Irmão" + Keys.ENTER)
+
+# Espera um segundo para a conversa ser carregada
+time.sleep(1)
+
+# Encontra o campo de mensagem e envia a mensagem desejada
+message_box = driver.find_element_by_xpath('//div[@class="_2_1wd copyable-text selectable-text"][@contenteditable="true"][@data-tab="1"]')
+message_box.send_keys("Teste Python" + Keys.ENTER)
+
+# Fecha o navegador após o envio da mensagem
+driver.quit()
