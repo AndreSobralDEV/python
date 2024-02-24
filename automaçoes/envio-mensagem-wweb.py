8c38cf6b24659eebcf184fd1675e53e3d694ea3e
AndreSobralDEV <159955496+AndreSobralDEV@users.noreply.github.com>
2024-02-24 11:09:19 -0300

    Update envio-mensagem-wweb.py

--- automaçoes/envio-mensagem-wweb.py
+++ automaçoes/envio-mensagem-wweb.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 # Inicializa o driver do Chrome (certifique-se de ter o ChromeDriver instalado)
-driver = webdriver.edge()
+driver = webdriver.Edge()
 
 # Abre o WhatsApp Web
 driver.get("https://web.whatsapp.com/")
